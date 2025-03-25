147b915086485ae21cd86ded2471ea896afadbb9
playtime-1967 <samannamnik@proton.me>
2025-03-25 21:41:21 +0000

    two-sum

---
+++ two-pointer/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums:list[int], target:int) -> list[int]:
+        store = {}
+        for (i,num) in enumerate(nums) :
+            num2 = target - num
+            if num2 in store :
+                return [store[num2], i]
+            store[num] = i
+
+        return [-1,-1]    
+
+        
+
