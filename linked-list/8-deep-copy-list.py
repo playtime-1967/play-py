2a2802427017762e62ab97b74609252b95b9e108
playtime-1967 <samannamnik@proton.me>
2025-03-28 08:54:17 +0000

    implement deep copy of a node list including next ref and random ref

---
+++ linked-list/8-deep-copy-list.py
@@ -0,0 +1,32 @@
+# remove the nth node from the end of the list and return its head.
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def deepCopyRandomList(self, head: Node) -> Node:
+
+        if not head:
+            return None
+
+        current = head
+        map = {}
+
+        while current:
+            newNode = Node(current.val)
+            map[current] = newNode
+            current = current.next
+
+        current = head
+
+        while current:
+            newNode = map[current]
+            newNode.next = map[current.next] if current.next else None
+            newNode.random = map[current.random] if current.random else None
+            current = current.next
+
+        return map[head]
