8c8be65d464047c0e977c797237cf858de7cef06
playtime-1967 <samannamnik@proton.me>
2025-04-06 14:09:38 +0100

    Binary Tree Level Order Traversal

---
+++ trees/13-level-order-traversal.py
@@ -0,0 +1,41 @@
+
+# Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path
+# equals targetSum.
+# A leaf is a node with no children.
+from TreeNode import TreeNode, treeA
+from collections import deque
+
+
+class Solution:
+
+    def levelOrder(self, root: TreeNode) -> any:
+        if not root:
+            return []
+
+        result = []
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            n = len(queue)
+
+            for _ in range(n):
+                node = queue.popleft()
+
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+
+        return result
+
+
+s = Solution()
+result = s.levelOrder(treeA)
+print("levelOrder: ", result)
